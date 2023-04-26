db4496af0df65c452fbac12a56424d84517fd4cc
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-26 10:05:26 -0400

    Formatted the codes to the ES6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import Web3 from "web3";
 
-import contractAbi from "../contractAbi/Web3ClubsToken.json"
-const contractAddress = "0x058733ecFf0EC820FeF767fa196108FF7e427E21"
+import contractAbi from "../contractAbi/Web3ClubsToken.json";
+const contractAddress = "0x058733ecFf0EC820FeF767fa196108FF7e427E21";
 
 export default function Home() {
   const [account, setAccount] = useState(null);
-  const [contractData, setContractData] = useState()
-  const [token_Name, setTokenName] = useState(null)
+  const [contractData, setContractData] = useState();
+  const [token_Name, setTokenName] = useState(null);
   const [recipientAddress, setRecipientAddress] = useState("");
   const [transferAmount, setTransferAmount] = useState(0);
 
@@ -21,63 +21,58 @@ export default function Home() {
   useEffect(() => {
     const getAccount = async () => {
       try {
-        const web3 = new Web3(window.ethereum)
-        const accounts = await web3.eth.getAccounts()
+        const web3 = new Web3(window.ethereum);
+        const accounts = await web3.eth.getAccounts();
 
-        console.log("Metamask Wallets", accounts)
+        console.log("Metamask Wallets", accounts);
 
-        setAccount(accounts[0])
+        setAccount(accounts[0]);
       } catch (error) {
         if (error.message === "User denied account authorizatio")
-          console.log("User denied account authorization")
+          console.log("User denied account authorization");
         else if (error.message === "MetaMask is not enabled") {
           // add link to download metamask
-          console.log(error)
+          console.log(error);
         } else {
-          console.log
+          console.log;
         }
       }
-    }
+    };
 
-    getAccount()
-    .catch(console.error)
-  }, [])
+    getAccount().catch(console.error);
+  }, []);
 
   //contract
   const getContractData = async () => {
-    try{
-      const web3 = new Web3(window.ethereum)
-      const tokenData = new web3.eth.Contract(
-        contractAbi.abi,
-        contractAddress
-      )
+    try {
+      const web3 = new Web3(window.ethereum);
+      const tokenData = new web3.eth.Contract(contractAbi.abi, contractAddress);
       //set token name
-      const balance = await tokenData.methods.balanceOf(account).call()
+      const balance = await tokenData.methods.balanceOf(account).call();
 
+      console.log(tokenData);
+      console.log(balance);
 
-      console.log(tokenData)
-      console.log(balance)
-
-      setContractData(tokenData)
-    } catch (error){
+      setContractData(tokenData);
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   // get contract name
   const getContractName = async () => {
-  try {
-    if (contractData) {
-      const token_Name = await contractData.methods.token_Name().call();
-      console.log(token_Name);
-      setTokenName(token_Name);
-    } else {
-      console.log("Contract data is not available yet");
+    try {
+      if (contractData) {
+        const token_Name = await contractData.methods.token_Name().call();
+        console.log(token_Name);
+        setTokenName(token_Name);
+      } else {
+        console.log("Contract data is not available yet");
+      }
+    } catch (error) {
+      console.log("Error:", error);
     }
-  } catch (error) {
-    console.log("Error:", error);
-  }
-};
+  };
 
   /**
    * The function handles a transfer of tokens using the Web3 library and logs the transaction hash and
@@ -85,60 +80,89 @@ export default function Home() {
    */
   const handleTransfer = async () => {
     try {
-      const web3 = new Web3(window.ethereum)
-      const tokenData = new web3.eth.Contract(
-        contractAbi.abi,
-        contractAddress
-      )
-      const tx = await tokenData.methods.transfer(recipientAddress, transferAmount).send({ from: account })
-      console.log(tx)
-      const txHash = tx.transactionHash
-      const txURL = `https://sepolia.etherscan.io/tx/${txHash}`
-      console.log(txURL)
+      const web3 = new Web3(window.ethereum);
+      const tokenData = new web3.eth.Contract(contractAbi.abi, contractAddress);
+      const transferAmountWei = web3.utils.toWei(
+        transferAmount.toString(),
+        "ether"
+      ); // convert to wei
+      const tx = await tokenData.methods
+        .transfer(recipientAddress, transferAmountWei)
+        .send({ from: account });
+      console.log(tx);
+      const txHash = tx.transactionHash;
+      const txURL = `https://sepolia.etherscan.io/tx/${txHash}`;
+      console.log(txURL);
+      document.getElementById("tx-url").innerHTML = txURL; // update tx-url div content
     } catch (error) {
-      console.log("You have not place all the parameters", error);
+      console.log("You have not placed all the parameters", error);
     }
-  }
+  };
 
   return (
     <div className="container min-h-screen flex-col items-center justify-between p-24">
-
       {!account ? (
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
-              onClick={() => window.ethereum.enable()}>Connet To MetaMask</button>
-
-      ): null}
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+          onClick={() => window.ethereum.enable()}
+        >
+          Connet To MetaMask
+        </button>
+      ) : null}
 
       {account ? (
         <div className="flex flex-col items-center">
           <h1 className="text-4xl font-bold">Your Account</h1>
           <p className="text-2xl">{account}</p>
 
-          <button onClick={getContractData} className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded">
-          Get Token Data
+          <button
+            onClick={getContractData}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+          >
+            Get Token Data
           </button>
           <br />
-          <button onClick={getContractName} className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded">
-          Display the Token Name
+          <button
+            onClick={getContractName}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+          >
+            Display the Token Name
           </button>
           {/* display token name here*/}
-          <h1 className="text-4xl font-bold">
-          {token_Name}
-          </h1>
-
-          </div>
-      ): null}
+          <h1 className="text-4xl font-bold">{token_Name}</h1>
+        </div>
+      ) : null}
       <br />
       <div className="flex flex-col items-center">
-        <label>Recipient Address</label><br />
-          <input className="border border-gray-400 p-2 mb-2 rounded-lg w-64 text-red-700" type="text" placeholder="Recipient Address" value={recipientAddress} onChange={(event) => setRecipientAddress(event.target.value)}
-          />
-          <br />
-        <label>Transfer Amount</label><br />
-          <input className="border border-gray-400 p-2 mb-2 rounded-lg w-64 text-blue-500" type="number"  placeholder="Transfer Amount"  value={transferAmount} onChange={(event) => setTransferAmount(event.target.value)} />
-          <br />
-          <button onClick={handleTransfer} className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded">Transfer Tokens</button>
-  </div>
-</div>
-  )
+        <label>Recipient Address</label>
+        <br />
+        <input
+          className="border border-gray-400 p-2 mb-2 rounded-lg w-64 text-red-700"
+          type="text"
+          placeholder="Recipient Address"
+          value={recipientAddress}
+          onChange={(event) => setRecipientAddress(event.target.value)}
+        />
+        <br />
+        <label>Transfer Amount</label>
+        <br />
+        <input
+          className="border border-gray-400 p-2 mb-2 rounded-lg w-64 text-blue-500"
+          type="number"
+          placeholder="Transfer Amount"
+          value={transferAmount}
+          onChange={(event) => setTransferAmount(event.target.value)}
+        />
+        <br />
+        <button
+          onClick={handleTransfer}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+        >
+          Transfer Tokens
+        </button>
+        <br />
+        <div id="tx-url"></div>
+      </div>
+    </div>
+  );
 }
